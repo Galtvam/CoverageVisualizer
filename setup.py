83927c517a8c315ecc14dd33d6775ab39db6f654
Galtvam <matg@cin.ufpe.br>
2022-04-08 04:34:24 -0300

    Format changed to Pytest plugin

---
+++ setup.py
@@ -0,0 +1,31 @@
+__author__ = ['Galtvam', 'vgss', 'fekete1']
+
+__license__ = 'MIT'
+__version__ = '0.0.0'
+
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name='coverage-visualizer',
+    version=__version__,
+    author=__author__,
+    description='Pytest plugin to visualize the coverage of your tests',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Galtvam/CoverageVisualizer',
+    packages=['coverage_visualizer'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Framework :: Pytest'
+    ],
+    entry_points={
+        'pytest11': ['coverage_visualizer = coverage_visualizer.plugin']
+    },
+    install_requires=['pytest'],
+    python_requires='>=3.7',
+)
