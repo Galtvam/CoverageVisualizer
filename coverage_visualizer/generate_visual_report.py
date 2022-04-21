709ba4e1c76920437bba397164cb766185b27f16
Sheldon Fekete <jsbf@cin.ufpe.br>
2022-04-21 04:35:09 -0300

    create and adjusts html page generation with jinja2

--- coverage_visualizer/generate_visual_report.py
+++ coverage_visualizer/generate_visual_report.py
@@ -1,9 +1,26 @@
+from jinja2 import FileSystemLoader, Environment
+import os
+import webbrowser
+
 class VisualReportGenerator:
     def __init__(self):
         pass
     
     @staticmethod
     def generate_visual_report(total_statements, total_executed_statements):
-        print(' Total statements: ' + str(total_statements))
-        print(' Total executed statements: ' + str(total_executed_statements))
-        print(' Total Coverage: ' + str((total_executed_statements/total_statements)*100) + '%')
+        
+        loader = FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates'))
+        env = Environment(loader=loader)
+        template = env.get_template("template.html")
+
+        os.makedirs("output", exist_ok=True)
+        file = open("output/index.html", "w")
+
+        render = template.render(total_statements= total_statements, executed_statements= total_executed_statements,
+                        total_coverage= str((total_executed_statements/total_statements)*100) + '%')
+
+        file.write(render)
+        file.close()
+
+        path= os.path.abspath("output/index.html")
+        webbrowser.open("file://"+path)
