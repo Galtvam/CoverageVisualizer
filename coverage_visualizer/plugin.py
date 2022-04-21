89b5bc726ebadefdfc857f6d4607da981933591f
Galtvam <matg@cin.ufpe.br>
2022-04-21 16:06:38 -0300

    adjusts to return a dict of coverage

--- coverage_visualizer/plugin.py
+++ coverage_visualizer/plugin.py
@@ -16,6 +16,7 @@ class CoverageVisualizerPlugin:
         self.__tracers = []
         self.__test_files_statements = {}
         self.__count_executed_statements = {}
+        self.__coverage_result = {}
 
     def pytest_pyfunc_call(self, pyfuncitem):
         test_function = pyfuncitem.obj
@@ -29,13 +30,14 @@ class CoverageVisualizerPlugin:
             tracer.runfunc(test_function, *args, **kwargs)
 
         pyfuncitem.obj = tracer_wrapper
-        if not str(pyfuncitem.fspath) in self.__test_files_statements.keys():
-            self.__test_files_statements[str(pyfuncitem.fspath)] = count_statements(str(pyfuncitem.fspath))
+        #if not str(pyfuncitem.fspath) in self.__test_files_statements.keys():
+        #    executed_statements = count_statements(str(pyfuncitem.fspath))
+        #    self.__test_files_statements[str(pyfuncitem.fspath)] = executed_statements
 
 
     def pytest_sessionfinish(self, session, exitstatus):
-        total_statements = count_statements(APPLICATION_DIRECTORY)
-        total_statements -= sum(self.__test_files_statements.values())
+        #total_statements = count_statements(APPLICATION_DIRECTORY)
+        #total_statements -= sum(self.__test_files_statements.values())
     
         for i, tracer in enumerate(self.__tracers):
             if i == 0:
@@ -45,10 +47,13 @@ class CoverageVisualizerPlugin:
         
         if results:
             results.write_results(coverdir=DEFAULT_REPORTS_DIRECTORY)
-            get_executed_statements(DEFAULT_REPORTS_DIRECTORY, self.__count_executed_statements)
-            
-        total_executed_statements = count_executed_statements(self.__count_executed_statements)
-        VisualReportGenerator.generate_visual_report(total_statements, total_executed_statements)
+            get_executed_statements(DEFAULT_REPORTS_DIRECTORY, self.__count_executed_statements, self.__coverage_result)
+        total_statements = count_statements(APPLICATION_DIRECTORY, self.__coverage_result)
+        
+        total_executed_statements, self.__coverage_result = count_executed_statements(self.__count_executed_statements, self.__coverage_result)
+        
+        self.__coverage_result['final_coverage'] = {"total_stataments":total_statements, "executed_statements":total_executed_statements}
+        VisualReportGenerator.generate_visual_report(self.__coverage_result)
 
 
 def pytest_configure(config):
